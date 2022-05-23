294fad1780681cecf5a69654624b308cd78dab98
Tanya <tanaybrown@gmail.com>
2022-05-22 17:43:38 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,93 +1,94 @@
-function isArray(collection) {
-    let newArr;
-    if (Array.isArray(collection)) {
-        newArr = collection;
-    } else {
-        newArr = Object.values(collection);
-    } 
-return newArr;
+function isArray(col) {
+  let newArr;
+  if (Array.isArray(col)) {
+    newArr = col;
+  } else {
+    newArr = Object.values(col);
+  }
+  return newArr;
 }
 
 function myEach(collection, callback) {
-    let newArr = isArray(collection);
-   
-    for(let i = 0; i < newArr.length; i++){
-        callback(newArr[i]);
-}
-
-return collection;
-}
-
-function myMap(collection, callback) {
-    let newArr = isArray(collection);
-    const answer = {}
-    for (let i = 0; i < newArr.length; i++) {
-        let final = callback(newArr[i]);
-       answer.push(final);
-}
-return answer;
-}
-
-function myReduce(collection, callback, acc) {
-    let newArr = isArray(colleciton)
-    for (let i = 0; i < newArr.length; i++) {
-        if (acc === undefined) {
-            acc = newArr[0];
-            i = 1;
-        }
-        return acc;
-    }
-}
-
-function myFind(collection, predicate) {
-    let newArr = isArray(collection);
-    let answer;
-    for (let i = 0; i < newArr.length; i++) {
-        if (predicate(collection[i])) {
-            answer = collection[i];
-            return answer;
-        }
-    }
-    return answer;
-}
-function myFilter(collection, predicate) {
-    let newArr = isArray(collection);
-    let finArr = [];
-    for (let i = 0; i < newArr.length; i++) {
-      if (predicate(collection[i])) {
-        finArr.push(collection[i]);
-      }
-    }
-    return finArr;
-}
-function mySize(collection) {
-    let newArr = isArray(collection);
-    return newArr.length;
+  let newArr = isArray(collection);
+  for (let i = 0; i < newArr.length; i++) {
+    callback(newArr[i]);
   }
-  
-  function myFirst(arr, n) {
-    if (n === undefined) {
-      return arr[0];
-    } else if (arr.length > 1) {
-      return arr.splice(0, n);
+  return collection;
+}
+
+function myMap(col, cb) {
+  let newArr = isArray(col);
+  const anuda = [];
+  for (let i = 0; i < newArr.length; i++) {
+    let final = cb(newArr[i]);
+    anuda.push(final);
+  }
+  return anuda;
+}
+
+function myReduce(col, cb, acc) {
+  let newArr = isArray(col);
+  for (let i = 0; i < newArr.length; i++) {
+    if (acc === undefined) {
+      acc = newArr[0];
+      i = 1;
+    }
+    acc = cb(acc, newArr[i], col);
+  }
+  return acc;
+}
+
+function myFind(col, predicate) {
+  let newArr = isArray(col);
+  let answer;
+  for (let i = 0; i < newArr.length; i++) {
+    if (predicate(col[i])) {
+      answer = col[i];
+      return answer;
     }
   }
+  return answer;
+}
 
-  function myLast(arr, n) {
-    let copy = arr.slice();
-    if (n === undefined) {
-      return copy.pop();
-    } else {
-      return copy.slice(-n);
+function myFilter(col, pred) {
+  let newArr = isArray(col);
+  let finArr = [];
+  for (let i = 0; i < newArr.length; i++) {
+    if (pred(col[i])) {
+      finArr.push(col[i]);
     }
   }
+  return finArr;
+}
 
-  function myKeys(obj) {
-    let answer = Object.keys(obj);
-    return answer;
+function mySize(col) {
+  let newArr = isArray(col);
+  return newArr.length;
+}
+
+function myFirst(arr, n) {
+  if (n === undefined) {
+    return arr[0];
+  } else if (arr.length > 1) {
+    return arr.splice(0, n);
   }
-  
-  function myValues(obj) {
-    let answer = Object.values(obj);
-    return answer;
+}
+
+function myLast(arr, n) {
+  let copy = arr.slice();
+  if (n === undefined) {
+    return copy.pop();
+  } else {
+    return copy.slice(-n);
   }
+}
+
+function myKeys(obj) {
+  let answer = Object.keys(obj);
+  return answer;
+}
+
+function myValues(obj) {
+  let answer = Object.values(obj);
+  return answer;
+}
